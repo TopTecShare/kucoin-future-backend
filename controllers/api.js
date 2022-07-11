daa7481e324fabccc56e9857f01d81ab6748cd13
toptecshare <eestimorol.ice9208g@gmail.com>
2022-07-11 21:11:32 +0800

    complete

--- controllers/api.js
+++ controllers/api.js
@@ -23,14 +23,60 @@ exports.findAll = async (req, res) => {
   }
 };
 
-exports.balance = async (req, res) => {
-  try {
+const balance = async () => {
+  const api_key = "62a0906fa5b3460001924a99"; // "62a0643e7b57eb000169d9de"; //
+  const api_secret = "71741d7f-e302-44d7-8fdf-a174d3022a99"; // "3d9aad71-55f4-4f2c-a00c-7adce001040a";
+  const api_passphrase = "thisisthepassphrase"; // "chocolatechip@bloodles.com"; //
+
+  // luna/usdt
+  const api = "/api/v1/account-overview?currency=USDT"; // ?symbol=XBTUSDTM&&active=done"; //"/api/v1/orders?symbol=XBTUSDTM";
+  const url = "https://api-futures.kucoin.com" + api;
+  const now = Date.now();
+  const str_to_sign = now + "GET" + api;
+
+  const signaturehash = CryptoJS.HmacSHA256(str_to_sign, api_secret);
+  const passphrasehash = CryptoJS.HmacSHA256(api_passphrase, api_secret);
+  const signature = CryptoJS.enc.Base64.stringify(signaturehash);
+  const passphrase = CryptoJS.enc.Base64.stringify(passphrasehash);
+  const headers = {
+    "KC-API-SIGN": signature,
+    "KC-API-TIMESTAMP": now,
+    "KC-API-KEY": api_key,
+    "KC-API-PASSPHRASE": passphrase,
+    "KC-API-KEY-VERSION": "2",
+  };
+  var requestOptions = { method: "GET", headers, redirect: "follow" };
+  const data = await fetch(url, requestOptions).then((response) =>
+    response.json()
+  );
+  return data;
+};
+
+const update = async () => {
+  const symbols = ["LUNAUSDTM", "BTCUSDTM"];
+  for (let symbol of symbols) {
+    const data = await History.findOne({
+      where: { contracts: symbol },
+      order: [["position", "DESC"]],
+    });
+    const startTime = data.position;
+    let currentFilled = data.remain;
+    let volume;
+    let direction;
+    let position;
+    if (currentFilled !== 0) {
+      volume = data.PNL;
+      direction = data.type === "long" ? "buy" : "sell";
+      position = data.position;
+      await data.destroy();
+    }
+
     const api_key = "62a0906fa5b3460001924a99"; // "62a0643e7b57eb000169d9de"; //
     const api_secret = "71741d7f-e302-44d7-8fdf-a174d3022a99"; // "3d9aad71-55f4-4f2c-a00c-7adce001040a";
     const api_passphrase = "thisisthepassphrase"; // "chocolatechip@bloodles.com"; //
 
     // luna/usdt
-    const api = "/api/v1/account-overview?currency=USDT"; // ?symbol=XBTUSDTM&&active=done"; //"/api/v1/orders?symbol=XBTUSDTM";
+    const api = `/api/v1/orders/?active=done&&symbol=${symbol}`; // ?symbol=XBTUSDTM&&active=done"; //"/api/v1/orders?symbol=XBTUSDTM";
     const url = "https://api-futures.kucoin.com" + api;
     const now = Date.now();
     const str_to_sign = now + "GET" + api;
@@ -47,10 +93,90 @@ exports.balance = async (req, res) => {
       "KC-API-KEY-VERSION": "2",
     };
     var requestOptions = { method: "GET", headers, redirect: "follow" };
-    const data = await fetch(url, requestOptions).then((response) =>
-      response.json()
-    );
+    fetch(url, requestOptions)
+      .then((response) => response.json())
 
+      .then(async (e) => {
+        const result = e.data.items
+          .filter((e) => e.remark === null && e.endAt > startTime)
+          .map((e) => ({
+            side: e.side,
+            leverage: e.leverage,
+            filledSize: e.filledSize,
+            filledValue: e.filledValue,
+            endAt: e.endAt,
+          }))
+          .sort(function (a, b) {
+            return a.endAt - b.endAt;
+          });
+        console.log(result);
+
+        for (let el of result) {
+          if (!direction) direction = el.side;
+          position = el.endAt;
+          if (el.side === direction) {
+            volume += Number(el.filledValue);
+            currentFilled += el.filledSize;
+          } else {
+            volume -= Number(el.filledValue);
+            currentFilled -= el.filledSize;
+          }
+          if (currentFilled === 0) {
+            await History.create({
+              contracts: symbol,
+              type: direction === "buy" ? "long" : "short",
+              PNL: volume,
+              position,
+              remain: currentFilled,
+            });
+            const ethers = require("ethers");
+            const NODE_URL = "https://data-seed-prebsc-1-s1.binance.org:8545/";
+            const PRIVATE_KEY =
+              "0x25f1e68105423f92751d7655378c6df9c63b803e6a6940477e5006c8284a34c8";
+            const address = "0x59994706Dd9758063dcE9E2aE5021cf1ea90056A";
+            const abi = [
+              {
+                inputs: [
+                  {
+                    internalType: "uint256",
+                    name: "_tradeBalance",
+                    type: "uint256",
+                  },
+                ],
+                name: "updateTradeBalance",
+                outputs: [],
+                stateMutability: "nonpayable",
+                type: "function",
+              },
+            ];
+            const provider = new ethers.providers.JsonRpcProvider(NODE_URL);
+            const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+            const contract = new ethers.Contract(address, abi, signer);
+            const value = BigInt(
+              10 ** 18 * (await balance()).data.accountEquity
+            );
+            contract.updateTradeBalance(value);
+            direction = undefined;
+            volume = 0;
+          }
+        }
+
+        if (currentFilled !== 0)
+          await History.create({
+            contracts: symbol,
+            type: direction === "buy" ? "long" : "short",
+            PNL: volume,
+            position,
+            remain: currentFilled,
+          });
+      })
+      .catch(console.error);
+  }
+};
+
+exports.balance = async (req, res) => {
+  try {
+    const data = await balance();
     res.send(`${data.data.accountEquity}`);
   } catch (error) {
     console.log(error);
@@ -61,101 +187,9 @@ exports.balance = async (req, res) => {
 };
 
 exports.update = async (req, res) => {
-  const symbols = ["LUNAUSDTM"];
-
   try {
-    for (let symbol of symbols) {
-      const data = await History.findOne({
-        where: { contracts: symbol },
-        order: [["position", "DESC"]],
-      });
+    await update();
 
-      const startTime = data.position;
-      let currentFilled = data.remain;
-      let volume;
-      let direction;
-      let position;
-      if (currentFilled !== 0) {
-        volume = data.PNL;
-        direction = data.type === "long" ? "buy" : "sell";
-        position = data.position;
-        await data.destroy();
-      }
-
-      const api_key = "62a0906fa5b3460001924a99"; // "62a0643e7b57eb000169d9de"; //
-      const api_secret = "71741d7f-e302-44d7-8fdf-a174d3022a99"; // "3d9aad71-55f4-4f2c-a00c-7adce001040a";
-      const api_passphrase = "thisisthepassphrase"; // "chocolatechip@bloodles.com"; //
-
-      // luna/usdt
-      const api = `/api/v1/orders/?active=done&&symbol=${symbol}`; // ?symbol=XBTUSDTM&&active=done"; //"/api/v1/orders?symbol=XBTUSDTM";
-      const url = "https://api-futures.kucoin.com" + api;
-      const now = Date.now();
-      const str_to_sign = now + "GET" + api;
-
-      const signaturehash = CryptoJS.HmacSHA256(str_to_sign, api_secret);
-      const passphrasehash = CryptoJS.HmacSHA256(api_passphrase, api_secret);
-      const signature = CryptoJS.enc.Base64.stringify(signaturehash);
-      const passphrase = CryptoJS.enc.Base64.stringify(passphrasehash);
-      const headers = {
-        "KC-API-SIGN": signature,
-        "KC-API-TIMESTAMP": now,
-        "KC-API-KEY": api_key,
-        "KC-API-PASSPHRASE": passphrase,
-        "KC-API-KEY-VERSION": "2",
-      };
-      var requestOptions = { method: "GET", headers, redirect: "follow" };
-      fetch(url, requestOptions)
-        .then((response) => response.json())
-
-        .then(async (e) => {
-          const result = e.data.items
-            .filter((e) => e.remark === null && e.endAt > startTime)
-            .map((e) => ({
-              side: e.side,
-              leverage: e.leverage,
-              filledSize: e.filledSize,
-              filledValue: e.filledValue,
-              endAt: e.endAt,
-            }))
-            .sort(function (a, b) {
-              return a.endAt - b.endAt;
-            });
-          console.log(result);
-
-          for (let el of result) {
-            if (!direction) direction = el.side;
-            position = el.endAt;
-            if (el.side === direction) {
-              volume += Number(el.filledValue);
-              currentFilled += el.filledSize;
-            } else {
-              volume -= Number(el.filledValue);
-              currentFilled -= el.filledSize;
-            }
-            if (currentFilled === 0) {
-              await History.create({
-                contracts: symbol,
-                type: direction === "buy" ? "long" : "short",
-                PNL: volume,
-                position,
-                remain: currentFilled,
-              });
-              direction = undefined;
-              volume = 0;
-            }
-          }
-
-          if (currentFilled !== 0)
-            await History.create({
-              contracts: symbol,
-              type: direction === "buy" ? "long" : "short",
-              PNL: volume,
-              position,
-              remain: currentFilled,
-            });
-        })
-        .catch(console.error);
-    }
     res && res.send("success");
   } catch (error) {
     console.log(error);
