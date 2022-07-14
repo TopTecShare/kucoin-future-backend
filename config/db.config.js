0eae1cac738bd9ca4f24ef3ffb4dea4f1c6b56b6
Fabio Pineda <fabiopineda97@gmail.com>
2022-07-14 15:38:18 -0400

    .env file for configuration

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "citystate",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
   dialect: "mysql",
   pool: {
     max: 5,
